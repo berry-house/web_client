125ecab72d19d410bb6a7c74efc3bfc901bb1299
Alejandro Güereca Valdivia <ale_guereca_valdivia@hotmail.com>
2018-03-15 19:42:39 -0600

    3 commit presentation
    Signed-off-by: Alejandro Güereca Valdivia <ale_guereca_valdivia@hotmail.com>

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ mysql = require('mysql');
 
 router.get('/', (req, res) => {
   req.getConnection((error, conn) => {
-    conn.query("SELECT id, name, (SELECT DATE_FORMAT(time, \"%Y/%m/%d %T\")) as time, lightIntensity, soilHumidity, soilTemperature, airTemperature from plant, conditions where id = plantID", (err, result, fields) => {
+    conn.query("SELECT id, name, (SELECT DATE_FORMAT(time, \"%Y/%m/%d %T\")) as time, lightIntensity, soilHumidity, soilTemperature, airTemperature from plant, conditions where id = plantID  order by id, time DESC", (err, result, fields) => {
       if (err) throw err;
       console.log(result);
       let string = JSON.stringify(result);
